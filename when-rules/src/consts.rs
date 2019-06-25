fdde0eb9986ccf3c27ba2958ba36c4219ddce3f0
Boris Tatarintsev <ttyv00@gmail.com>
2019-06-25 18:32:41 +0300

    Remove old files

--- when-rules/src/consts.rs
+++
@@ -1,5 +0,0 @@
-pub(crate) const SECOND: i32 = 1;
-pub(crate) const MINUTE: i32 = 60 * SECOND;
-pub(crate) const HOUR: i32 = 60 * MINUTE;
-pub(crate) const DAY: i32 = 24 * HOUR;
-pub(crate) const WEEK: i32 = 7 * DAY;
